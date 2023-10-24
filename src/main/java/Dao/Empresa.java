9d05402508922eeb9ab60bd2b423a25c085afd4b
Gonzalo <gonzalo.rubio.gutierrez@gmail.com>
2023-10-24 12:46:45 +0200

    Sql-2

--- src/main/java/Dao/Empresa.java
+++ src/main/java/Dao/Empresa.java
@@ -8,11 +8,40 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 import Model.Departamento;
 
 public class Empresa {
 	
+	
+	public static Boolean insertEmpleado(Connection connection, String nombre, Double salario, LocalDate nacido, Departamento departamento) {
+        try {
+            Statement statement = (Statement) connection.createStatement();
+            String query = "Insert INTO Empleados ( nombre, salario, nacido, departamento) VALUES (" + nombre + ", " + salario + ", " + nacido + ", " + departamento + ")";
+            ResultSet resultSet = ((java.sql.Statement) statement).executeQuery(query);
+            resultSet.close();
+            ((Connection) statement).close();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+	
+	public static Boolean insertDepartamento(Connection connection, String nombre, Empleado jefe) {
+        try {
+            Statement statement = (Statement) connection.createStatement();
+            String query = "Insert INTO Departamentos ( nombre, jefe) VALUES (" + nombre + ", " + jefe + ")";
+            ResultSet resultSet = ((java.sql.Statement) statement).executeQuery(query);
+            resultSet.close();
+            ((Connection) statement).close();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 	
 	public static void selectAllFromTable(Connection connection, String tableName) {
         try {
@@ -39,14 +68,12 @@ public class Empresa {
             Statement statement = (Statement) connection.createStatement();
             String query = "SELECT * FROM " + tableName + " where id = " + ID;
             ResultSet resultSet = ((java.sql.Statement) statement).executeQuery(query);
-
             while (resultSet.next()) {
                 // Aquí puedes recuperar los datos de cada fila y procesarlos según tus necesidades.
                 // Ejemplo: String columna1 = resultSet.getString("nombre_columna1");
                 //         int columna2 = resultSet.getInt("nombre_columna2");
                 //         // Realiza las operaciones que desees con los datos.
             }
-
             resultSet.close();
             ((Connection) statement).close();
         } catch (SQLException e) {
@@ -54,19 +81,21 @@ public class Empresa {
         }
     }
 	
-	public static void eliminarFromTable(Connection connection, String tableNameDel, String tableNameNull, int ID) {
+	public static Boolean eliminarFromTable(Connection connection, String tableNameDel, String tableNameNull, int ID) {
         try {
             Statement statement = (Statement) connection.createStatement();
             String query1 = "Update " + tableNameNull + " set " + tableNameDel.toLowerCase() + " = 'NULL' where " + tableNameDel.toLowerCase() + 
-            				" = SELECT nombre FROM " + tableNameDel + " where id = " + ID   ;
+            				" = (SELECT nombre FROM " + tableNameDel + " where id = " + ID + ")"   ;
             String query2 = "Delete * FROM " + tableNameDel + " where id = " + ID;
             ResultSet resultSet1 = ((java.sql.Statement) statement).executeQuery(query1);
             ResultSet resultSet2 = ((java.sql.Statement) statement).executeQuery(query2);
             resultSet1.close();
             resultSet2.close();
             ((Connection) statement).close();
+            return true;
         } catch (SQLException e) {
             e.printStackTrace();
+            return false;
         }
     }
 
